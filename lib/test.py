3e77988e5d755108e3ba2dad61ec44330fc218cf
James Allen <jwlallen@gmail.com>
2024-08-25 00:34:15 +0100

    prototyped index & ticker retrieval

--- lib/test.py
+++
@@ -1,9 +0,0 @@
-from credentials.SeldonDBCredentials import *
-from credentials.EodHistoricalDataCredentials import *
-
-
-print(eod_api)
-print(db_password)
-print(db_uname)
-print(db_server_address)
-
